234289a049fe3ad5a2a7668ede5b010fb23585a6
MANAS MADESHIYA <159601988+MANAS225@users.noreply.github.com>
2024-11-13 20:06:55 +0530

    Add files via upload

---
+++ pot-1/script.js
@@ -0,0 +1,10 @@
+var typed= new Typed(".text",{
+    strings:["Frontend Developer", "Coder", "Web Developer", "Programmer"],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:500,
+    loop:true
+});
+document.addEventListener('DOMContentLoaded', function() {
+    typed.reset();
+});
